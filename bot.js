328887cdc185ebd64c5b4b4791409e57d393aba9
JeremiahHarris19 <75698932+JeremiahHarris19@users.noreply.github.com>
2020-12-10 16:56:00 -0500

    new replies

--- bot.js
+++ bot.js
@@ -5,26 +5,38 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOTTOKEN);
-
+const CHANNEL_ID = "785990104366841897"
 client.on('ready', readyDiscord);
 
 function readyDiscord() {
-  console.log('happy birthday winter babies');
+  console.log('happy winter');
+  console.log(getReply())
+  // client.channels.cache
+  //   .get(CHANNEL_ID)
+  //   .send(getReply())
 }
 
-const replies = [
-  'December 24',
-  'check the calendar!!!',
-  'ask jq',
-  'Christmas eve',
-  'uuhhhh ya know what i mean',
+const replies = [{
+    name: 'Christmas eve',
+    date: 'December 24',
+  },
+  {
+    name: 'Christmas',
+    date: 'December 25',
+  }
 ]
 
 client.on('message', gotMessage);
 
-function gotMessage(msg){
-  if (msg.channel.id == '785990104366841897'  && msg.content === 'when is break') {
-    const index = Math.floor(Math.random() * replies.length);
-    msg.reply(replies[index]);
+function gotMessage(msg) {
+  if (msg.channel.id == CHANNEL_ID && msg.content === 'when is break') {
+
+    msg.reply(getReply())
   }
+}
+
+function getReply() {
+  const index = Math.floor(Math.random() * replies.length);
+  const date = replies[index]
+  return date['name'] + ' on ' + date['date'];
 }
