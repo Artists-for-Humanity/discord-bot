bf24a68d43299262412532f972ac2fdb8d31c12b
Stephen Cronin <cronin4392@gmail.com>
2021-01-07 13:19:40 -0500

    Improve database setup scripts

--- data/setup.js
+++ data/setup.js
@@ -5,31 +5,67 @@ const dbExists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
 
+const insertHolidaySql = (name, date) =>
+  `INSERT INTO Holidays (name, startDate) VALUES ("${name}", ${date})`;
+
+const insertMeetingSql = (link, date, name) =>
+  `INSERT INTO Meetings (zoomLink, date, name) VALUES ("${link}", ${date}, "${name}")`;
+
 // if the database does not exist, create it, otherwise print records to console
 db.serialize(() => {
   if (!dbExists) {
-    db.run(
-      "CREATE TABLE Holidays (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, startDate INT)"
-    );
-    db.run(
-      "CREATE TABLE Meetings (id INTEGER PRIMARY KEY AUTOINCREMENT, zoomLink TEXT, date INT, name TEXT)"
-    );
-    console.log("New table Holidays created!");
-
-    // insert some dates
     db.serialize(() => {
+      // Create tables
       db.run(
-        'INSERT INTO Holidays (name, startDate) VALUES ("Christmas", 1608854400)'
+        "CREATE TABLE Holidays (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, startDate INT)",
+        (err) => {
+          if (err) {
+            console.error("Error creating Holidays table");
+            console.error(err);
+          }
+          console.log("New table Holidays created!");
+        }
       );
       db.run(
-        'INSERT INTO Meetings (zoomLink, date, name) VALUES ("https://zoom.us/j/95454828935?pwd=QWlkUktxRWttRHpLOUw3ei9lajBHdz09", 1607990400, "December 15th at 5:30pm w/ Special Guest Joy Triche")'
-      );
-      db.run(
-        'INSERT INTO Meetings (zoomLink, date, name) VALUES ("https://zoom.us/j/95454828935?pwd=QWlkUktxRWttRHpLOUw3ei9lajBHdz09", 1607385600, "December 8th at 5:30pm w/ Special Guest Joy Triche")'
-      );
-      db.run(
-        'INSERT INTO Meetings (zoomLink, date, name) VALUES ("https://zoom.us/j/95454828935?pwd=QWlkUktxRWttRHpLOUw3ei9lajBHdz09", 1608658200, "December 22nd at 5:30pm w/ Special Guest Joy Triche")'
+        "CREATE TABLE Meetings (id INTEGER PRIMARY KEY AUTOINCREMENT, zoomLink TEXT, date INT, name TEXT)",
+        (err) => {
+          if (err) {
+            console.error("Error creating Meetings table");
+            console.error(err);
+          }
+          console.log("New table Meetings created!");
+        }
       );
     });
+
+    // Insert Data
+    db.serialize(() => {
+      db.run(insertHolidaySql("Christmas", 1608854400));
+      db.run(
+        insertMeetingSql(
+          "https://zoom.us/j/95454828935?pwd=QWlkUktxRWttRHpLOUw3ei9lajBHdz09",
+          1607990400,
+          "December 15th at 5:30pm w/ Special Guest Joy Triche"
+        )
+      );
+      db.run(
+        insertMeetingSql(
+          "https://zoom.us/j/95454828935?pwd=QWlkUktxRWttRHpLOUw3ei9lajBHdz09",
+          1607385600,
+          "December 8th at 5:30pm w/ Special Guest Joy Triche"
+        )
+      );
+      db.run(
+        insertMeetingSql(
+          "https://zoom.us/j/95454828935?pwd=QWlkUktxRWttRHpLOUw3ei9lajBHdz09",
+          1608658200,
+          "December 22nd at 5:30pm w/ Special Guest Joy Triche"
+        )
+      );
+    });
+  } else {
+    console.warn(
+      "Database already exists, please run 'npm run reset-db' before running setup."
+    );
   }
 });
