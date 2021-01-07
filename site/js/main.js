be89925cd5bbd08863c8c5afd09bee97348db82b
Marhvin <76072394+Marhvin@users.noreply.github.com>
2021-01-07 17:26:41 -0500

    added flatpickr instead of default

--- site/js/main.js
+++ site/js/main.js
@@ -1,1 +1,8 @@
-console.log("This runs in the browser");
+import flatpickr from "flatpickr";
+
+const datePickers = document.querySelector(".date-picker");
+
+flatpickr(datePickers, {
+  enableTime: true,
+  dateFormat: "Y-m-d H:i",
+});
