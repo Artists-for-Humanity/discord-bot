e4b71f1d9322f37cdbc39e13c359a60180147983
Marhvin <76072394+Marhvin@users.noreply.github.com>
2021-01-07 17:26:41 -0500

    added flatpickr instead of default

--- site/js/main.js
+++ site/js/main.js
@@ -1,7 +1,15 @@
+import flatpickr from "flatpickr";
 console.log("This runs in the browser");
 const button = document.querySelector('.toggle-darkmode');
 const body = document.body;
 
 button.addEventListener('click', event => {
   body.classList.toggle("dark-mode");
+});
+// If using flatpickr in a framework, its recommended to pass the element directly
+const datePickers = document.querySelector(".date-picker")
+
+flatpickr(datePickers, {
+  enableTime: true,
+  dateFormat: "Y-m-d H:i",
 });
